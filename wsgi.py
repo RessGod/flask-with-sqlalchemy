fdd8fd7d37a8c9079db86c936279bb06afb8e914
Sergino RIBA <serginoriba155@gmail.com>
2022-03-17 14:53:41 +0300

    Add get and POST

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 from config import Config
 app = Flask(__name__)
@@ -17,7 +17,7 @@ ma = Marshmallow(app)
 from models import Product
 migrate = Migrate(app,db)
 
-from schemas import many_product_schema
+from schemas import many_product_schema, one_product_schema
 
 @app.route('/hello', methods=['GET'])
 def hello():
@@ -27,3 +27,16 @@ def hello():
 def get_many_product():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return many_product_schema.jsonify(products), 200
+
+@app.route(f'{BASE_URL}/products/<id>', methods=['GET'])
+def get_one_product(id):
+    products = db.session.query(Product).get(id) # SQLAlchemy request => 'SELECT * FROM products WHERE id = id'
+    return one_product_schema.jsonify(products), 200
+
+@app.route(f'{BASE_URL}/products', methods=['POST'])
+def add_product():
+    product = Product() # SQLAlchemy request => 'INSERT INTO products (name) VALUES ('Skello')'
+    product = request.get_json()
+    db.session.add(product)
+    db.session.commit()
+    return one_product_schema.jsonify(product), 200
